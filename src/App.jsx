f2bc0053ae6de2d828b52f59111f08314ac0dfad
purecodework <68151429+purecodework@users.noreply.github.com>
2022-04-18 19:39:06 +1000

    add torch support

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,39 @@
 import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
 function App() {
   const [value, setValue] = useState(0);
   const [err, setError] = useState("");
-  const handleScan = (data) => setValue(data);
-  const handleError = (err) => console.log(err);
+  const [torch, setTorch] = useState(false);
+  const handleUpdate = (err, res) => {
+    res ? setValue(res.text) : setError(err);
+  };
 
   return (
-    <div className="App">
-      start
-      <BarcodeScannerComponent
-        width={500}
-        height={500}
-        onUpdate={(err, result) => {
-          if (result) setValue(result.text);
-          else setErr(err);
-        }}
-      />
-      end
-      <p>{value}</p>
-      <p>{err}</p>
+    <div className="App h-screen w-screen flex justify-center items-center bg-slate-300">
+      <div>
+        <div className="border-indigo-500/75">
+          <BarcodeScannerComponent
+            width={500}
+            height={500}
+            onUpdate={(err, result) => handleUpdate}
+            torch={torch}
+          />
+        </div>
+        <div className="text-center mt-5">
+          <>
+            <button
+              onClick={() => setTorch(!torch)}
+              className="text-white bg-blue-700 p-2 rounded"
+            >
+              torch
+            </button>
+          </>
+          <p className="text-indigo-500 mt-5">bar code: {value}</p>
+          <p className="text-red-300">error: {err}</p>
+        </div>
+      </div>
     </div>
   );
 }
